5b3a2b98978ca3cb3ffcf9e34e6eaa5b3a442889
lluanaa <lstylik13@gmail.com>
2023-11-03 00:09:56 -0300

    primeiro commit

---
+++ curso-de-js/curso-de-js-aula-03/aula01.js
@@ -0,0 +1,38 @@
+document.write("Estudando no feriado")
+console.log("outro ola mundo")
+
+let mensagem = "minha mensagem"
+let mensagem2 = "outra mensagem"
+let meuPeso = 83.5
+let minhaAltura = 1.75
+let minhaIdade = 40
+let ehDoador = false
+let teste = null
+
+/* CRIAR CONSTANTES E ATRIBUIR VALOR */
+const PI = 3.145
+const TAXA = 0.5
+
+teste = "algum conteudo"
+
+/* ESCREVER O CONTEUDO DE VARIAVEIS E CONSTANTES, POREM ESCREVE TUDO JUNTO
+document.write("mensagem")
+document.write("mensagem2")
+document.write("meuPeso")
+document.write("minhaAltura")
+document.write("minhaIdade")
+document.write("ehDoador")
+document.write("teste")
+*/
+
+/* ESCREVER O CONTEUDO DE VARIAVEIS E CONSTANTES */
+
+document.write("<p> Mensagem " + mensagem + "</p>")
+document.write("Mensagem2: " + mensagem2)
+document.write("<p>Peso: " + meuPeso + "kg <p>")
+document.write("Altura: " + minhaAltura)
+document.write("<p> Idade: " + minhaIdade + " anos</p>")
+document.write("Doador: " + ehDoador)
+document.write("<p>Teste: " + teste)
+document.write("<p>PI: " + PI)
+document.write("<p>TAXA: " + TAXA)
---
+++ curso-de-js/curso-de-js-aula-03/aula03.js
@@ -0,0 +1,30 @@
+let n1 = 10 // number inteiro
+let n2 = 2 // number inteiro
+let n3 = "2" // string inteiro, no caso um texto, pq ta em ""
+
+document.write("n1")
+document.write("n2")
+document.write("n3")
+
+console.log(n1 + n2) //adicao
+console.log(n1 - n2) //subtracao
+console.log(n1 * n2) //multiplicacao
+console.log(n1 / n2) // divisao
+console.log(n1 % n2) //resto ou modulo
+
+console.log(n2 == n3) // igual a
+console.log(n2 === n3) // identico a 
+console.log(n2 != n3) // diferente de nao =
+console.log(n2 !== 3) // nao e identico
+
+console.log(n2 > n2)
+console.log(n2 === n3)
+console.log(n2 != n3)
+console.log(n2 !== n3)
+let a = true
+let b = false
+
+console.log(a && b) // E AND
+console.log(a || b ) // OU OR
+console.log(!a) // NOT OU NEGAÇÃO
+console.log(!b)
